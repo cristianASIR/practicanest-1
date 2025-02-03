2b02edf43a75726c2cba8dfbd986aadbfe7d47b5
cristianasir2 <manzcris@gmail.com>
2025-02-03 18:56:28 +0100

    .

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,12 +4,13 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Pokemon } from './entities/pokemon.entity';
 import { Repository } from 'typeorm';
+import { error } from 'console';
 
 @Injectable()
 export class PokemonService {
   //Conexión a la base de datos
   constructor(
-    @InjectRepository(Pokemon,'base2')
+    @InjectRepository(Pokemon, 'base2')
     private pokemonRepository: Repository<Pokemon>
   ) { }
   //Primeras funciones asincronas que utilizamos
@@ -25,6 +26,9 @@ export class PokemonService {
 
   async findOne(id: number): Promise<Pokemon> {
     const pokemon = await this.pokemonRepository.findOne({ where: { id } })
+    if (!pokemon) {
+      throw new Error(`Pokemon con ID #${id} no se encuentra`);
+    }
     return pokemon;
   }
 
@@ -34,24 +38,35 @@ export class PokemonService {
     return this.pokemonRepository.save(pokemon);
   }
 
-  async remove(id: number):Promise<void> {
+  async remove(id: number): Promise<void> {
     const pokemon = await this.findOne(id);
     await this.pokemonRepository.remove(pokemon);
   }
-  async findByTipo(tipo:string):Promise<Pokemon|void>{
-    return await this.pokemonRepository.findOne({where:{tipo}});
+  async findByTipo(tipo: string): Promise<Pokemon | void> {
+    const pokemon = await this.pokemonRepository.findOne({ where: { tipo } });
+    if (!pokemon) {
+      throw new Error(`Pokémon de tipo ${tipo} no existe.`);
+    }
+    return pokemon;
   }
-  async activatePokemon(id:number):Promise<Pokemon>{
-    const pokemon=await this.pokemonRepository.findOne({where:{id}});
-    pokemon.activo=true;
+
+  async activatePokemon(id: number): Promise<Pokemon> {
+    const pokemon = await this.pokemonRepository.findOne({ where: { id } });
+    if (!pokemon) {
+      throw new Error(`Pokémon con ID ${id} no encontrado.`);
+    }
+    pokemon.activo = true;
     return await this.pokemonRepository.save(pokemon);
   }
-  async deactivatePokemon(id:number):Promise<Pokemon>{
-    const pokemon=await this.pokemonRepository.findOne({where:{id}});
-    pokemon.activo=false;
+  async deactivatePokemon(id: number): Promise<Pokemon> {
+    const pokemon = await this.pokemonRepository.findOne({ where: { id } });
+    if (!pokemon) {
+      throw new Error(`Pokémon con ID ${id} no encontrado.`);
+    }
+    pokemon.activo = false;
     return await this.pokemonRepository.save(pokemon);
   }
-  async buscaNombre(nombre:string):Promise<Pokemon[]>{
-    return this.pokemonRepository.find({where:{nombre}})
+  async buscaNombre(nombre: string): Promise<Pokemon[]> {
+    return this.pokemonRepository.find({ where: { nombre } })
   }
 }
