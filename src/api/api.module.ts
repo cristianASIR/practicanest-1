1acbdd44362e82bec26f041b11d403d1b870b1dc
cristianasir2 <manzcris@gmail.com>
2025-02-05 17:53:22 +0100

    .

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -3,10 +3,11 @@ import { ApiService } from './api.service';
 import { ApiController } from './api.controller';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   controllers: [ApiController],
   providers: [ApiService],
-  imports: [PostModule, UserModule],
+  imports: [PostModule, UserModule, PostsModule],
 })
 export class ApiModule {}
